d23892b2d98030a7f4c7958c6670111c49820b2f
chocolaLain <lainundalice@gmail.com>
2018-11-19 23:14:22 +0800

    Site updated: 2018-11-19 23:14:05

--- js/src/custom.js
+++ js/src/custom.js
@@ -6,8 +6,8 @@ function randomColor() {
 /* 轮播背景图片 */
 $(function () {
     $.backstretch([  
-          "http://pi67irekk.bkt.clouddn.com/18-11-15/4737610.jpg",
-          "http://pi67irekk.bkt.clouddn.com/18-11-15/62187172.jpg",
+          "https://tsundora.com/image/2014/06/strike_witches_435-1024x666.jpg",
+          "https://blog-001.west.edge.storage-yahoo.jp/res/blog-af-dc/blacksisterred/folder/1199014/67/36181567/img_0",
           "http://pi67irekk.bkt.clouddn.com/18-11-17/37361663.jpg"
     ], { duration: 6000, fade: 1500 });  
 });
