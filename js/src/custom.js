f4e6b5a1b5254dcf9eed01974d3ba47c251170d8
chocolaLain <lainundalice@gmail.com>
2018-11-21 23:59:29 +0800

    Site updated: 2018-11-21 23:59:10

--- js/src/custom.js
+++ js/src/custom.js
@@ -6,8 +6,8 @@ function randomColor() {
 /* 轮播背景图片 */
 $(function () {
     $.backstretch([  
-          "https://tsundora.com/image/2014/06/strike_witches_435-1024x666.jpg",
-          "https://blog-001.west.edge.storage-yahoo.jp/res/blog-af-dc/blacksisterred/folder/1199014/67/36181567/img_0",
-          "http://pi67irekk.bkt.clouddn.com/18-11-17/37361663.jpg"
+          "http://pi67irekk.bkt.clouddn.com/18-11-21/81996400.jpg",
+          "http://pi67irekk.bkt.clouddn.com/18-11-17/37361663.jpg",
+          "http://pi67irekk.bkt.clouddn.com/18-11-21/9424912.jpg"
     ], { duration: 6000, fade: 1500 });  
 });
