da639f8b632bf34370ea3780aa3f890fd34739b4
chocolaLain <lainundalice@gmail.com>
2018-11-24 23:42:30 +0800

    Site updated: 2018-11-24 23:42:17

--- js/src/custom.js
+++ js/src/custom.js
@@ -4,18 +4,18 @@ function randomColor() {
 }
 
 /* 轮播背景图片 */
-/*$(function () {
+$(function () {
     $.backstretch([  
           "http://pi67irekk.bkt.clouddn.com/18-11-21/81996400.jpg",
           "http://pi67irekk.bkt.clouddn.com/18-11-17/37361663.jpg",
           "http://pi67irekk.bkt.clouddn.com/18-11-21/9424912.jpg"
     ], { duration: 6000, fade: 1500 });  
-});*/
+});
 /* 备用轮播背景图片 */
-$(function () {
+/*$(function () {
     $.backstretch([  
           "http://pi67irekk.bkt.clouddn.com/18-11-21/9424912.jpg",
           "http://pi67irekk.bkt.clouddn.com/18-11-21/73456748.jpg",
           "http://pi67irekk.bkt.clouddn.com/18-11-15/49794517.jpg"
     ], { duration: 6000, fade: 1500 });  
-});
+});*/
