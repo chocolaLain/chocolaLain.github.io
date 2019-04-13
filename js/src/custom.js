869966e419581e4413ebff1314d138b92d77fd43
chocolaLain <lainundalice@gmail.com>
2019-04-13 14:15:00 +0800

    Site updated: 2019-04-13 14:14:57

--- js/src/custom.js
+++ js/src/custom.js
@@ -6,16 +6,9 @@ function randomColor() {
 /* 轮播背景图片 */
 $(function () {
     $.backstretch([  
-          "http://lainundalice.oss-cn-beijing.aliyuncs.com/19-1-6/70711331.jpg",
-          "http://lainundalice.oss-cn-beijing.aliyuncs.com/18-12-1/4001987.jpg",
-          "https://images3.alphacoders.com/558/thumb-1920-558013.jpg"
+          "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00258.jpg",
+          "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00189.jpg",
+          "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00002.jpg",
     ], { duration: 6000, fade: 1500 });  
 });
-/* 备用轮播背景图片 */
-/*$(function () {
-    $.backstretch([  
-          "http://pi67irekk.bkt.clouddn.com/18-11-21/9424912.jpg",
-          "http://pi67irekk.bkt.clouddn.com/18-11-21/73456748.jpg",
-          "http://pi67irekk.bkt.clouddn.com/18-11-15/49794517.jpg"
-    ], { duration: 6000, fade: 1500 });  
-});*/
+
