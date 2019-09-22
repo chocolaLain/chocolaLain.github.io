c2c6bdac988480e020b042ae71ec95ccb34479f2
chocolaLain <lainundalice@gmail.com>
2019-09-23 01:14:00 +0800

    Site updated: 2019-09-23 01:13:58

--- js/src/custom.js
+++ js/src/custom.js
@@ -5,10 +5,17 @@ function randomColor() {
 
 /* 轮播背景图片 */
 $(function () {
-    $.backstretch([  
-          "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00525.jpg",
-          "/images/ACG.HD.00184.jpg",
-          "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00073.jpg",
+    $.backstretch([ 
+        // 私用
+        "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00525.jpg",
+        "/images/ACG.HD.00184.jpg",
+        "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00073.jpg",
+
+        // 公共场合用
+        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00780.jpg",
+        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/CachedImage_1366_768_POS4.jpg",
+        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00661.jpg",
     ], { duration: 6000, fade: 1500 });  
 });
+
 
