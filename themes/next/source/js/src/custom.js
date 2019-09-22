71af0dcfe0579c0a071734c969969c6f97e2db81
chocolaLain <lainundalice@gmail.com>
2019-09-23 01:13:21 +0800

    updated the notes about 3ds max, photograph

--- themes/next/source/js/src/custom.js
+++ themes/next/source/js/src/custom.js
@@ -7,14 +7,14 @@ function randomColor() {
 $(function () {
     $.backstretch([ 
         // 私用
-        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00525.jpg",
-        // "/images/ACG.HD.00184.jpg",
-        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00073.jpg",
+        "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00525.jpg",
+        "/images/ACG.HD.00184.jpg",
+        "https://lainundalice.oss-cn-beijing.aliyuncs.com/ACG.HD.00073.jpg",
 
         // 公共场合用
-        "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00780.jpg",
-        "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/CachedImage_1366_768_POS4.jpg",
-        "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00661.jpg",
+        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00780.jpg",
+        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/CachedImage_1366_768_POS4.jpg",
+        // "https://lainundalice.oss-cn-beijing.aliyuncs.com/img/ACG.HD.00661.jpg",
     ], { duration: 6000, fade: 1500 });  
 });
 
